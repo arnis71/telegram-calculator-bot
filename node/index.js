289bef880b1dcddb9881a9fba192771f768c93ad
Arnis Shaykh <arnis71@gmail.com>
2018-03-01 18:00:42 +0500

    fix index.js path, add Marco polo test

---
+++ node/index.js
@@ -0,0 +1,47 @@
+(function (_, Kotlin) {
+  'use strict';
+  var contains = Kotlin.kotlin.text.contains_li3zpu$;
+  var to = Kotlin.kotlin.to_ujzrz7$;
+  var json = Kotlin.kotlin.js.json_pyyo18$;
+  var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var Unit = Kotlin.kotlin.Unit;
+  function main$lambda$lambda(closure$res) {
+    return function (f) {
+      println('Message posted');
+      return closure$res.end('ok');
+    };
+  }
+  function main$lambda$lambda_0(closure$res) {
+    return function (err) {
+      println('Error : ' + err);
+      return closure$res.end('Error : ' + err);
+    };
+  }
+  function main$lambda(closure$axios) {
+    return function (req, res) {
+      var message = req.body.message;
+      if (!contains(message.text.toString(), 'marco', true)) {
+        return res.end();
+      }
+      return closure$axios.post('https://api.telegram.org/518559990:AAHp7scR3FUcXYLit3cH8I6YEC3KpNrqfc4/sendMessage', json([to('chat_id', message.chat.id), to('text', 'Polo!!')])).then(main$lambda$lambda(res)).catch(main$lambda$lambda_0(res));
+    };
+  }
+  function main$lambda_0() {
+    println('Telegram app listening on port 3000!');
+    return Unit;
+  }
+  function main(args) {
+    var express = require('express');
+    var parser = require('body-parser');
+    var axios = require('axios');
+    var app = express();
+    app.use(parser.json());
+    app.use(parser.urlencoded());
+    app.get('/new-message', main$lambda(axios));
+    app.listen(3000, main$lambda_0);
+  }
+  _.main_kand9s$ = main;
+  main([]);
+  Kotlin.defineModule('index', _);
+  return _;
+}(module.exports, require('kotlin')));
--- src/main/kotlin/Index.kt
+++ src/main/kotlin/Index.kt
@@ -1,2 +1,38 @@
+import kotlin.js.json
+
 fun main(args: Array<String>) {
+
+    val express = require("express")
+    val parser = require("body-parser")
+    val axios = require("axios")
+
+    val app = express()
+
+    app.use(parser.json())
+    app.use(parser.urlencoded())
+
+    app.get("/new-message") { req, res ->
+        val message = req.body.message
+
+        if (!message.text.toString().contains("marco", true)) {
+            return@get res.end()
+        }
+
+        axios.post("https://api.telegram.org/518559990:AAHp7scR3FUcXYLit3cH8I6YEC3KpNrqfc4/sendMessage",
+            json(
+                "chat_id" to message.chat.id,
+                "text" to "Polo!!"
+            )
+        ).then { _ ->
+            println("Message posted")
+            res.end("ok")
+        }.catch { err ->
+            println("Error : $err")
+            res.end("Error : $err")
+        }
+    }
+
+    app.listen(3000) {
+        println("Telegram app listening on port 3000!")
+    }
 }
