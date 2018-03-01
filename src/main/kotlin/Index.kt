06c7555888846ee6bbe3317d287b170c69eb4d99
Arnis Shaykh <arnis71@gmail.com>
2018-03-01 21:05:48 +0500

    further debugging

--- src/main/kotlin/Index.kt
+++ src/main/kotlin/Index.kt
@@ -9,34 +9,27 @@ fun main(args: Array<String>) {
     val app = express()
 
     app.use(parser.json())
-    app.use(parser.urlencoded(
-        json(
-            "extended" to true
-        )
-    ))
-
-    app.get("/") { req, res ->
-        res.send("Hello world")
-    }
+    app.use(parser.urlencoded(json("extended" to true)))
 
     app.get("/new-message") { req, res ->
-        val message = req.body.message
-
-        if (!message.text.toString().contains("marco", true)) {
-            return@get res.end()
-        }
-        axios.post("https://api.telegram.org/518559990:AAHp7scR3FUcXYLit3cH8I6YEC3KpNrqfc4/sendMessage",
-            json(
-                "chat_id" to message.chat.id,
-                "text" to "Polo!!"
-            )
-        ).then { _ ->
-            println("Message posted")
-            res.end("ok")
-        }.catch { err ->
-            println("Error : $err")
-            res.end("Error : $err")
-        }
+        res.send("ok")
+//        val message = req.body.message
+//
+//        if (!message.text.toString().contains("marco", true)) {
+//            return@get res.end()
+//        }
+//        axios.post("https://api.telegram.org/518559990:AAHp7scR3FUcXYLit3cH8I6YEC3KpNrqfc4/sendMessage",
+//            json(
+//                "chat_id" to message.chat.id,
+//                "text" to "Polo!!"
+//            )
+//        ).then { _ ->
+//            println("Message posted")
+//            res.end("ok")
+//        }.catch { err ->
+//            println("Error : $err")
+//            res.end("Error : $err")
+//        }
     }
 
     app.listen(3000) {
