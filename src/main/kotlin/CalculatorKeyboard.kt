386d24782ce525bc8371a6e69e577f37b2cde71c
Arnis Shaykh <arnis71@gmail.com>
2018-03-02 18:17:49 +0500

    change keyboard type: reply -> inline

--- src/main/kotlin/CalculatorKeyboard.kt
+++ src/main/kotlin/CalculatorKeyboard.kt
@@ -1,12 +1,12 @@
 import kotlin.js.json
 
-class CalculatorKeyboard(private val rows: Int, private val cols: Int, private val resizeKeyboard: Boolean = true) {
+class CalculatorKeyboard(private val rows: Int, private val cols: Int) {
     private val numbers = generateSequence(1) {
         it + 1
     }
 
     fun toJson() = json(
-        "keyboard" to sequenceOf(-1, 0, -1)
+        "inline_keyboard" to sequenceOf(-1, 0, -1)
             .plus(numbers.take((rows - 2) * cols))
             .map {
                 if (it == -1)
@@ -15,10 +15,12 @@ class CalculatorKeyboard(private val rows: Int, private val cols: Int, private v
                     it.toString()
             }
             .plus(sequenceOf("AC", "+", "-"))
-            .map { json("text" to it) }
+            .map { json(
+                "text" to it,
+                "callback_data" to it
+            ) }
             .chunked(cols)
             .toList()
-            .reversed(),
-        "resize_keyboard" to resizeKeyboard
+            .reversed()
     )
 }
